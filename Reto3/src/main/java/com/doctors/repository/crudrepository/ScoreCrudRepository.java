b51328f9f49a7fa9d41c02f278ff6bf81f147dba
Jose David Mejia Meneses <113116259+davidmejia1224@users.noreply.github.com>
2022-10-20 08:48:05 -0500

    se elimina carpeta Reto3

--- Reto3/src/main/java/com/doctors/repository/crudrepository/ScoreCrudRepository.java
+++
@@ -1,7 +0,0 @@
-package com.doctors.repository.crudrepository;
-
-import com.doctors.model.ScoreModel;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ScoreCrudRepository extends CrudRepository<ScoreModel, Integer> {
-}
--- Reto3/src/main/java/com/doctors/repository/crudrepository/UserAdministratorCrudRepository.java
+++
@@ -1,7 +0,0 @@
-package com.doctors.repository.crudrepository;
-
-import com.doctors.model.UserAdministratorModel;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserAdministratorCrudRepository extends CrudRepository<UserAdministratorModel, Integer> {
-}
