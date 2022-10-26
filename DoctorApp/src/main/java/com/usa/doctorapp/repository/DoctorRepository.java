577d5e80f8fda8c5bc7793c9452e03bfc6933f6a
Jose David Mejia Meneses <113116259+davidmejia1224@users.noreply.github.com>
2022-10-25 19:44:09 -0500

    creación de service en doctor, message, reservation y specialty

--- DoctorApp/src/main/java/com/usa/doctorapp/repository/DoctorRepository.java
+++ DoctorApp/src/main/java/com/usa/doctorapp/repository/DoctorRepository.java
@@ -30,5 +30,4 @@ public class DoctorRepository {
         doctorCrudRepository.delete(doctor);
 
     }
-
 }
--- DoctorApp/src/main/java/com/usa/doctorapp/service/MessageService.java
+++ DoctorApp/src/main/java/com/usa/doctorapp/service/MessageService.java
@@ -1,4 +1,70 @@
 package com.usa.doctorapp.service;
 
+import com.usa.doctorapp.model.Message;
+import com.usa.doctorapp.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MessageService {
+
+    @Autowired
+    private MessageRepository messageRepository;
+
+    public List<Message> getAll() {
+        return messageRepository.getAll();
+    }
+
+    public Optional<Message> getById(Integer id) {
+        return messageRepository.getById(id);
+    }
+
+    public Message save(Message message) {
+        if (message.getIdMessage() == null) {
+            return messageRepository.save(message);
+        } else {
+            Optional<Message> optionalMessage = messageRepository.getById(message.getIdMessage());
+            if (optionalMessage.isEmpty()) {
+                return messageRepository.save(message);
+            } else {
+                return message;
+            }
+        }
+    }
+
+    public  Message update(Message message) {
+        if (message.getIdMessage() != null) {
+            Optional<Message> optionalMessage = messageRepository.getById(message.getIdMessage());
+            if (!optionalMessage.isEmpty()) {
+                if (message.getMessageText() != null) {
+                    optionalMessage.get().setMessageText(message.getMessageText());
+                    optionalMessage.get().setClient(message.getClient());
+                    optionalMessage.get().setDoctor(message.getDoctor());
+                }
+                messageRepository.save(optionalMessage.get());
+                return optionalMessage.get();
+            } else {
+                return message;
+            }
+        } else {
+            return message;
+        }
+    }
+
+    public boolean delete(Integer id) {
+        Boolean aBoolean = getById(id).map(message -> {
+            messageRepository.delete(message);
+            return true;
+        }).orElse(false);
+        return aBoolean;
+    }
 }
+
+
+
+
+
+
